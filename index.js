370809041cdc2f98c29981cac8d85c616a89bc55
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-03 15:34:46 -0500

    .

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ $(document).ready(() => {
 
 
         if (languageSelector) {
-            const nav1 = document.getElementById('nav1');
+            /*const nav1 = document.getElementById('nav1');
             const nav2 = document.getElementById('nav2');
             const nav3 = document.getElementById('nav3');
             const slidePhrase1 = document.getElementById('slidePhrase1');
             const slidePhrase2 = document.getElementById('slidePhrase2');
-            const slidePhrase3 = document.getElementById('slidePhrase3');
+            const slidePhrase3 = document.getElementById('slidePhrase3');*/
 
             //de la pagina de configuracion
-            const welcome = document.getElementById('welcome');
+            /*const welcome = document.getElementById('welcome');
             const editDisponibility = document.getElementById('editDisponibility');
             const editDisponibility = document.getElementById('editDisponibility');
-            const editDisponibility = document.getElementById('editDisponibility');
+            const editDisponibility = document.getElementById('editDisponibility');*/
 
 
             const loadContent = (language) => {
